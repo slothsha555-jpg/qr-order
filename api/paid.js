d396e66aa0e2b7667e95b1b7a1649c00f4b18d99
Slothsha <slothsha555@gmail.com>
2026-02-07 16:25:41 +0700

    fix api paid gas url

--- api/paid.js
+++ api/paid.js
@@ -4,15 +4,12 @@ export default async function handler(req, res) {
   }
 
   try {
-    // ✅ ต้องใช้ googleusercontent.com เท่านั้น
     const GAS_URL =
-      "https://script.googleusercontent.com/macros/echo?user_content_key=PUT_YOUR_KEY_HERE";
+      "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjDw8O0AtZWQW7jbZGRTB94ClTCeFhmowrVWDrLfgNsGfCnAzoJbGbpOnquwfUSLazK0i9jlvyFtwxp0DWbb6JQy0y1fqWP0mFXCrtXlni28TGCidR7DQuoNROBb6M8iTQrs8CCb3bL930v6T6CHqTuCJA_VPjYuKiKcBLzwbPc2oA1R4k8kiks4AdPEYohR841-O0dmPaYXE3QfskqxNozWsZO8YL7yLj2OTul15IWReYy6iEGTsn_8k2tULsbMkwPpgQ_MYYXQqq-O4spAI6az1LtfHTIJvmtP-Ck&lib=MEh1kHHJB5xi5lKxnI9oL7k9OvfWI_fm-";
 
     const response = await fetch(GAS_URL, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
     });
 
