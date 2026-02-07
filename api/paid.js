57f849b47f58cf8fb8e18ae0104bd38f93c77575
Slothsha <slothsha555@gmail.com>
2026-02-07 16:34:23 +0700

    fix api paid use exec url

--- api/paid.js
+++ api/paid.js
@@ -1,21 +1,24 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+    return res.status(405).send("Method Not Allowed");
   }
 
   try {
     const GAS_URL =
-      "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLjDw8O0AtZWQW7jbZGRTB94ClTCeFhmowrVWDrLfgNsGfCnAzoJbGbpOnquwfUSLazK0i9jlvyFtwxp0DWbb6JQy0y1fqWP0mFXCrtXlni28TGCidR7DQuoNROBb6M8iTQrs8CCb3bL930v6T6CHqTuCJA_VPjYuKiKcBLzwbPc2oA1R4k8kiks4AdPEYohR841-O0dmPaYXE3QfskqxNozWsZO8YL7yLj2OTul15IWReYy6iEGTsn_8k2tULsbMkwPpgQ_MYYXQqq-O4spAI6az1LtfHTIJvmtP-Ck&lib=MEh1kHHJB5xi5lKxnI9oL7k9OvfWI_fm-";
+      "https://script.google.com/macros/s/AKfycbzmGR44z676R6brKDa5pwnP7mpgDWsWdznADerz0aiu3nuUqimKwyG97wkKWNY4qhFYxA/exec";
 
     const response = await fetch(GAS_URL, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(req.body),
     });
 
     const text = await response.text();
     res.status(200).send(text);
   } catch (err) {
-    res.status(500).json({ error: err.toString() });
+    console.error(err);
+    res.status(500).send("ERROR");
   }
 }
