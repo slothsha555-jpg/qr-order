548f7bd5f4fe454278e44f00e0b2c128e1a0fe0b
Slothsha <slothsha555@gmail.com>
2026-02-07 16:18:39 +0700

    fix paid post to gas

--- api/paid.js
+++ api/paid.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   try {
     const GAS_URL =
-      "https://script.googleusercontent.com/macros/echo?user_content_key=ใส่ของคุณตรงนี้";
+      "https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLh26kMXQPHmTJcDtjOZKMjoBrg6inehV_AUN3gbXPUiqAv0BTed-5IYSsEA0DcKzq7cAm_trBSTL6qVn4fG21nWwIv75jqbc3N3prixYUdjH4kI3b6deyKjKcMEm7DYC19-WXuzfCdFSpKsWZu7GkN5tSG1apGhKusxFUTQmkjQXFR8LwPfioCc8WiUUVMYIjYddpQYTGbQo7gCZ-F77w_dt-nAhm7rGuvWrihy7o1duTtD6JshP08kv-chJs1xfalGV8wADo8rTbipurB2IaA90n8KNWR7XTq5EJ5-&lib=MEh1kHHJB5xi5lKxnI9oL7k9OvfWI_fm-";
 
     const response = await fetch(GAS_URL, {
       method: "POST",
